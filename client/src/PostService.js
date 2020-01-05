67521bbad17bf46bde3ec4141bf84a0c6bcd9b2d
Jordan Foster <orehamakenai@gmail.com>
2020-01-05 19:40:06 +0900

    Added Front-End, Static, and SPA

---
+++ client/src/PostService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const url = 'api/posts/';
+
+class PostService {
+    //Get Posts
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try{
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                       ...post,
+                       createdAt: new Date(post.createdAt) 
+                    }))
+                );
+            } catch(err) {
+                reject(err);
+            }
+        })
+    }
+
+    //Create Post
+    static insertPosts(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+    //Delete Post
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`)
+    }
+}
+
+export default PostService;
